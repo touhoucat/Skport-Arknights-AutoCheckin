adf69ac92c9ffe19dcc451553fa96c8cdea9f6bd
touhoucat <76678469+touhoucat@users.noreply.github.com>
2026-05-14 17:48:57 +0800

    main

--- Skport_Arknights_AutoCheckin.ps1
+++ Skport_Arknights_AutoCheckin.ps1
@@ -1,7 +1,7 @@
-﻿# Skport-Arknights-AutoCheckin ( Windows / Linux / macOS )
+# Skport-Arknights-AutoCheckin ( Windows / Linux / macOS )
 
 # ── Settings ───────────────────────────────────────────
-$SK_OAUTH_CRED_KEY = "AAbbCC99Sk0aUtH1XXXCrED8KeY2kI8e" # your skport SK_OAUTH_CRED_KEY in cookie
+$SK_OAUTH_CRED_KEY = "AAbbCC99Sk0aUtH1XXXCrED8KeY2kI6e" # your skport SK_OAUTH_CRED_KEY in cookie
 $uid = "12345678"                                       # your Arknights game ID
 $server = "2"                                           # Asia=2 / Americas=3 / Europe=3
 $language = "zh_Hant"                                   # english=en / 繁體中文=zh_Hant / 简体中文=zh_Hans / 日本語=ja / 한국어=ko
